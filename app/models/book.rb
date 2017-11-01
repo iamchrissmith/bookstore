bddb388596b34ef60c2b5e68d4543772f32d2171
Chris Smith <me@chrisryansmith.com>
2017-11-01 11:51:41 -0600

    add required field validations for book

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,8 @@ class Book < ApplicationRecord
   has_many :book_format_types, through: :book_formats
 
   has_many :book_reviews
+
+  validates :title, presence: true
+  validates :publisher_id, presence: true
+  validates :author_id, presence: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Book, type: :model do
     it { should belong_to :publisher }
     it { should belong_to :author }
   end
+
+  context "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:publisher_id) }
+    it { should validate_presence_of(:author_id) }
+  end
 end
