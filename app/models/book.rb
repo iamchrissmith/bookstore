a580c31df8d9642a18e4575ea74e58d65a673df9
Chris Smith <me@chrisryansmith.com>
2017-11-02 08:19:47 -0600

    refactor search method into private methods for each search step

--- app/models/book.rb
+++ app/models/book.rb
@@ -20,29 +20,43 @@ class Book < ApplicationRecord
   end
 
   def self.search(query, options = {})
-    books = Book.joins(:book_reviews)
-                .select("books.*", "avg(book_reviews.rating) AS rating")
+    books = setup_book_search_query
 
     if options[:book_format_type_id] || options[:book_format_physical] != nil
-      books = books.joins(:book_format_types)
+      books = setup_book_format_query(books,options)
     end
 
-    if options[:book_format_physical] != nil
-      books = books.where("book_format_types.physical = ? ", options[:book_format_physical])
-    end
-
-    if options[:book_format_type_id]
-      books = books.where("book_format_types.id = ? ", options[:book_format_type_id])
-    end
-
-    if options[:title_only]
-      books = books.where("lower(title) LIKE ?", "%#{query.downcase}%")
-    else   
-      books = books.joins(:author, :publisher)
-                    .where("lower(authors.last_name) = :query OR lower(publishers.name) = :query OR lower(title) LIKE :fuzzy", 
-                          {query: query.downcase, fuzzy: "%#{query.downcase}%"})
-    end
-
-    books.group(:id).order("rating DESC")
+    execute_search(books, query, options).group(:id).order("rating DESC")
   end
+
+  private
+
+    def self.setup_book_search_query
+      joins(:book_reviews)
+      .select("books.*", "avg(book_reviews.rating) AS rating")
+    end
+
+    def self.setup_book_format_query(books, options)
+      books = books.joins(:book_format_types)
+
+      if options[:book_format_physical] != nil
+        books = books.where("book_format_types.physical = ? ", options[:book_format_physical])
+      end
+  
+      if options[:book_format_type_id]
+        books = books.where("book_format_types.id = ? ", options[:book_format_type_id])
+      end
+
+      books
+    end
+
+    def self.execute_search(books, query, options)
+      if options[:title_only]
+        books.where("lower(title) LIKE ?", "%#{query.downcase}%")
+      else   
+        books.joins(:author, :publisher)
+             .where("lower(authors.last_name) = :query OR lower(publishers.name) = :query OR lower(title) LIKE :fuzzy", 
+                   {query: query.downcase, fuzzy: "%#{query.downcase}%"})
+      end
+    end
 end
